fe43c3240e6f1a5bb90b7843da4af288f04352cf
Benjamin <benjaminayimah@gmail.com>
2023-09-28 17:07:42 +0000

    commit

---
+++ src/components/charts/BarChart.vue
@@ -0,0 +1,57 @@
+<template>
+    <Bar
+        id="my-chart-id"
+        :options="chartOptions"
+        :data="chartData"
+        :height="200"
+
+    />
+</template>
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+export default {
+    name: 'BarChart',
+    components: { Bar },
+    data() {
+        return {
+            chartData: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                datasets: [ 
+                    {
+                        label: 'Stats',
+                        data: [40, 20, 12, 20, 32, 50, 34, 22, 19, 25, 30, 45],
+                        backgroundColor: '#B58563',
+                        borderRadius: 40,
+                        borderSkipped: false,
+                    }
+                ]
+            },
+            chartOptions: {
+                responsive: true,
+                scales: {
+                    x: {
+                        grid: {
+                            display: false,
+                        },
+                        title: {
+                            display: true,
+                            text: 'Climbers',
+                        },
+                    },
+                    y: {
+                        grid: {
+                            display: false,
+                        },
+                        title: {
+                            display: true,
+                            text: 'Events',
+                        },
+                    }
+                }
+            }
+        }
+    }
+}
+</script>
--- src/views/admin/Statistics.vue
+++ src/views/admin/Statistics.vue
@@ -1,15 +1,37 @@
 <template>
-    <section>
-        <h1>Statistics</h1>
+    <section class="flx column gap-40 main">
+        <div class="grid gap-24 grid-col-4 dash-stats">
+            <dashboard-stat-list :color="'#E8E2FF'" />
+            <dashboard-stat-list :color="'#d5ffd5'" />
+            <dashboard-stat-list :color="'#e0f2fe'" />
+            <dashboard-stat-list :color="'#ffe4e6'" />
+        </div>
+        <div class="grid grid-col-2 gap-24 flx-1">
+            <div class="bg-white br-16 pd-32">
+                <h1>Stats</h1>
+                <div>
+                    <bar-chart />
+                </div>
+            </div>
+            <div class="bg-white br-16 pd-32">
+                <h1>Location stats</h1>
+                <bar-chart />
+            </div>
+        </div>
     </section>
 </template>
 
 <script>
+import DashboardStatList from '@/components/includes/DashboardStatList.vue'
+import BarChart from '@/components/charts/BarChart.vue'
 export default {
+  components: { DashboardStatList, BarChart },
     name: 'StatisticsView'
 }
 </script>
 
 <style lang="scss" scoped>
-
+section.main {
+    height: calc(100dvh - 128px);
+}
 </style>
