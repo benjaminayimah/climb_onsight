3456b442e9352dbc5e79dd4235109735ef0c1c60
Benjamin <benjaminayimah@gmail.com>
2023-09-30 01:05:05 +0000

    commit

--- src/views/guides/Calendar.vue
+++ src/views/guides/Calendar.vue
@@ -2,32 +2,7 @@
     <section class="main">
         <div class="bg-white br-24 flx section-main-wrapper pd-16 gap-16">
             <div class="calendar-left bg-color br-8 pd-32">
-                <h4 class="text-center mb-24">Add to your calender</h4>
-                <div class="flx column gap-24">
-                    <div class="bg-white br-16 cal-picker-wrapper">
-                        <vue-cal
-                        class="vuecal--rounded-theme vuecal--green-theme"
-                        xsmall
-                        hide-view-selector
-                        :time="false"
-                        active-view="month"
-                        :disable-views="['week']"
-                        @cell-click="handleDateClick"
-                        style="width: 100%;height: 300px; box-shadow: unset;">
-                    </vue-cal>
-                    </div>
-                    <div class="input-wrapper flx column gap-8">
-                        <label for="start_time">Start time</label>
-                        <input class="br-16" type="time" id="start_time" name="start_time" />
-                    </div>
-                    <div class="input-wrapper flx column gap-8">
-                        <label for="end_time">End time</label>
-                        <input class="br-16" type="time" id="end_time" name="end_time" />
-                    </div>
-                    <div>
-                        <button class="button-primary btn-md w-100">Next</button>
-                    </div>
-                </div>
+               left
             </div>
             <div class="calendar-right bg-color br-8 flx-1">
                 right
@@ -37,11 +12,11 @@
 </template>
 
 <script>
-import VueCal from 'vue-cal'
-import 'vue-cal/dist/vuecal.css'
+// import VueCal from 'vue-cal'
+// import 'vue-cal/dist/vuecal.css'
 export default {
     name: 'CalendarView',
-    components: { VueCal },
+    // components: { VueCal },
     methods: {
         handleDateClick(date) {
             console.log(date)
