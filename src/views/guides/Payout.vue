0dc6ded5a8a682393f0d5227de3af0f276715503
Benjamin <benjaminayimah@gmail.com>
2023-11-15 20:52:57 +0000

    commit

--- src/views/guides/Payout.vue
+++ src/views/guides/Payout.vue
@@ -178,7 +178,7 @@ export default {
         }
     },
     mounted() {
-        this.user.details_submitted && this.user.payouts_enabled ? this.fetchAccount() : ''
+        // this.user.details_submitted && this.user.payouts_enabled ? this.fetchAccount() : ''
     }
 }
 </script>
