63261bf8820473895fd69aaa263ac31e39c9bd4e
Benjamin <benjaminayimah@gmail.com>
2023-11-24 21:55:14 +0000

    commit

--- src/components/charts/BarChart.vue
+++ src/components/charts/BarChart.vue
@@ -2,13 +2,12 @@
     <Bar
         id="my-chart-id"
         :options="chartOptions"
-        :data="computedData"
+        :data="data"
         :height="200"
 
     />
 </template>
 <script>
-import moment from "moment"
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
@@ -16,36 +15,10 @@ export default {
     name: 'BarChart',
     components: { Bar },
     props: {
-        data: Array,
+        data: Object,
         title: String,
         xAxis: String,
         yAxis: String
-    },
-    computed: {
-        computedData() {
-            const chartData = {
-                labels: [],
-                datasets: [ 
-                    {
-                        label: this.title,
-                        data: [],
-                        backgroundColor: '#B58563',
-                        borderRadius: 40,
-                        borderSkipped: false,
-                    }
-                ]
-            }
-            this.data.forEach(element => {
-                let dateLabel = moment(element.created_at).format('MMM DD, YYYY')
-                let check = chartData.labels.find(data => data == dateLabel)
-                if(!check) {
-                    chartData.labels.push(dateLabel)
-                    let filterVal = this.data.filter(date => moment(date.created_at).format('MMM DD, YYYY') == moment(element.created_at).format('MMM DD, YYYY'))
-                    chartData.datasets[0].data.push(filterVal.length)
-                }
-            })
-            return chartData
-        }
     },
     data() {
         return {
--- src/views/admin/Statistics.vue
+++ src/views/admin/Statistics.vue
@@ -13,7 +13,7 @@
         <div class="grid grid-col-2 gap-24 flx-1">
             <div class="bg-white br-16 pd-32">
                 <div class="flx jc-sb ai-c">
-                    <h1>Sign ups</h1>
+                    <h1>New users</h1>
                     <ul class="flx bg-color pd-8 br-32 tab">
                         <li>
                             <router-link :to="{ name: 'Statistics'}" :class="{'active': $route.query.state !== 'guides'}">Climbers</router-link>
@@ -24,19 +24,20 @@
                     </ul>
                 </div>
                 <div>
-                    <bar-chart :data="climbers" :yAxis="'No. of Climbers'" :xAxis="'Time period'" :title="'Climbers signup'" v-if="$route.query.state !== 'guides'" />
-                    <bar-chart :data="guides" :yAxis="'No. of Guides'" :xAxis="'Time period'" :title="'Guides signup'" v-else/>
+                    <bar-chart :data="computedClimbers" :yAxis="'No. of Climbers'" :xAxis="'Time period'" v-if="$route.query.state !== 'guides'" />
+                    <bar-chart :data="computedGuides" :yAxis="'No. of Guides'" :xAxis="'Time period'" v-else/>
                 </div>
             </div>
             <div class="bg-white br-16 pd-32">
-                <h1>Bookings</h1>
-                <bar-chart :data="bookings" :yAxis="'Bookings'" :xAxis="'Time period'" :title="'Bookings'" />
+                <h1>Locations</h1>
+                <bar-chart :data="computedBookings" :yAxis="'No. of events'" :xAxis="'Locations'" />
             </div>
         </div>
     </section>
 </template>
 
 <script>
+import moment from "moment"
 import statsMixin from '@/mixins/statsMixin';
 import DashboardStatList from '@/components/includes/DashboardStatList.vue'
 import BarChart from '@/components/charts/BarChart.vue'
@@ -49,8 +50,81 @@ export default {
         ...mapState({
             bookings: (state) => state.bookings,
             climbers: (state) => state.climbers,
-            guides: (state) => state.guides
-        })
+            guides: (state) => state.guides,
+            events: (state) => state.events
+        }),
+        computedClimbers() {
+            const chartData = {
+                labels: [],
+                datasets: [ 
+                    {
+                        label: 'Climbers',
+                        data: [],
+                        backgroundColor: '#B58563',
+                        borderRadius: 40,
+                        borderSkipped: false,
+                    }
+                ]
+            }
+            this.climbers.forEach(element => {
+                let dateLabel = moment(element.created_at).format('MMM DD, YYYY')
+                let check = chartData.labels.find(data => data == dateLabel)
+                if(!check) {
+                    chartData.labels.push(dateLabel)
+                    let filterVal = this.climbers.filter(date => moment(date.created_at).format('MMM DD, YYYY') == moment(element.created_at).format('MMM DD, YYYY'))
+                    chartData.datasets[0].data.push(filterVal.length)
+                }
+            })
+            return chartData
+        },
+        computedGuides() {
+            const chartData = {
+                labels: [],
+                datasets: [ 
+                    {
+                        label: 'Guides',
+                        data: [],
+                        backgroundColor: '#B58563',
+                        borderRadius: 40,
+                        borderSkipped: false,
+                    }
+                ]
+            }
+            this.guides.forEach(element => {
+                let dateLabel = moment(element.created_at).format('MMM DD, YYYY')
+                let check = chartData.labels.find(data => data == dateLabel)
+                if(!check) {
+                    chartData.labels.push(dateLabel)
+                    let filterVal = this.guides.filter(date => moment(date.created_at).format('MMM DD, YYYY') == moment(element.created_at).format('MMM DD, YYYY'))
+                    chartData.datasets[0].data.push(filterVal.length)
+                }
+            })
+            return chartData
+        },
+        computedBookings() {
+            const chartData = {
+                labels: [],
+                datasets: [ 
+                    {
+                        label: 'Event locations',
+                        data: [],
+                        backgroundColor: '#B58563',
+                        borderRadius: 40,
+                        borderSkipped: false,
+                    }
+                ]
+            }
+            this.events.forEach(element => {
+                let dateLabel = element.address
+                let check = chartData.labels.find(data => data == dateLabel)
+                if(!check) {
+                    chartData.labels.push(dateLabel)
+                    let filterVal = this.events.filter(data => data.address  == element.address)
+                    chartData.datasets[0].data.push(filterVal.length)
+                }
+            })
+            return chartData
+        }
     }
 }
 </script>
