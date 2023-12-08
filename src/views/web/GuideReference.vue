c1206b420beff43bf5aae9100b5605a504b46c6a
Benjamin <benjaminayimah@gmail.com>
2023-12-08 01:40:22 +0000

    commit

--- src/views/web/GuideReference.vue
+++ src/views/web/GuideReference.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="stepper-wrapper w-100 flx column gap-24">
         <div class="stepper-title">Referees</div>
+        <div v-if="systemErr.error" class="invalid-credentials response-message text-center mb-32">
+            <span>{{ systemErr.message }}</span>
+        </div>
         <form @submit.prevent="" id="guide_ref_form">
             <div class="form-wrapper flx column gap-24">
                 <div>
