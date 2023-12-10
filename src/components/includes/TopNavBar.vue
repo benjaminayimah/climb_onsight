90dfdc6675fbe114e337a47e07ff2d00bdcbcb41
Benjamin <benjaminayimah@gmail.com>
2023-12-10 13:35:43 +0000

    commit

--- src/components/includes/TopNavBar.vue
+++ src/components/includes/TopNavBar.vue
@@ -13,7 +13,7 @@
             </div>
             <div class="flx gap-70">
                 <ul class="flx gap-16 ai-c">
-                    <button class="nav-dropdown-toggle br-50" @click="$store.dispatch('getAuthUser', token)">
+                    <button title="Refresh page" class="nav-dropdown-toggle br-50" @click="$store.dispatch('getAuthUser', token)">
                         <svg :class="{'rotate' : pageLoader }" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 23.999 24">
                             <path d="M26.475,9.525A11.99,11.99,0,1,0,18,30a11.751,11.751,0,0,0,11.362-8.281c-.008.036.138-.848-1.11-1.177A1.58,1.58,0,0,0,26.475,21,9,9,0,1,1,18,9a8.872,8.872,0,0,1,6.33,2.67l-3.649,3.694s-.276.53,0,.857a.953.953,0,0,0,.757.279H28.4a2.282,2.282,0,0,0,1.238-.532A1.979,1.979,0,0,0,30,14.734V8.355a.98.98,0,0,0-.365-.959c-.489-.32-1.115.111-1.115.111Z" transform="translate(-6.015 -6)" fill="#595959"/>
                         </svg>
